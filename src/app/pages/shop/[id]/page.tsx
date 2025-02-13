3f65e05625a980c610087c37a47bd96f9cdcbf63
solken32 <solistoke@gmail.com>
2025-02-13 09:58:24 -0500

    solution page props en npm run build

--- src/app/pages/shop/[id]/page.tsx
+++ src/app/pages/shop/[id]/page.tsx
@@ -2,12 +2,12 @@ import Layout from '@/components/layout';
 import SingleProduct from '@/components/ShopDetails';
 import React from 'react';
 
-export default function ProductDetail({ params }: { params: { id: string } }) {
-
+export default async function ProductDetail({ params, }: { params: Promise<{ id: string }> }) {
+  const  {id}  = await params
   return (
     <Layout>
-      <h1>Producto : { params.id}</h1>
-      <SingleProduct id={params.id} />
+      <h1>Producto : {id}</h1>
+      <SingleProduct id={id} />
     </Layout  >
 
   )
